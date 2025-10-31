0cfe521bce05c547e02ae4489bd0590e2d75bb8d
Richard L. Sypert Jr <rsypertjr@hotmail.com>
2025-10-31 16:46:53 -0700

    Enhance patient evaluation logic by adding blood pressure risk assessment, improving error handling for input values, and updating UI to display risk information.

--- app/show-patients/page.tsx
+++ app/show-patients/page.tsx
@@ -1,5 +1,6 @@
 // File: app/test-handler/page.tsx
 'use client';
+import { minify } from 'next/dist/build/swc/generated-native';
 import { COOKIE_NAME_PRERENDER_DATA } from 'next/dist/server/api-utils';
 import { useEffect, useState, useCallback } from 'react';
 
@@ -14,6 +15,13 @@ export type Patient = {
     diagnosis:string,
     medications:string
 };
+
+export type EvalPatient = Patient & {
+    blood_pressure_risk?:number,
+    temperature_risk?:number,
+    age_risk?:number,
+    total_risk_score?:number
+}
    
 const aPatient:Patient = {
     patient_id:'',
@@ -48,10 +56,16 @@ export default function TestHandler(){
     const [ patientIndex, setPatientIndex] = useState(0);
     const [ patientNumber, setPatientNumber] = useState(1);
     const [ totalPatients, setTotalPatients] = useState(0);
+    const [ infoError, setInfoError] = useState('');
+    const [ systolic, setSystolic ] = useState(0);
+    const [ diastolic, setDiastolic ] = useState(0);
     const [ patients, setPatients] = useState([]);
     const [ pageNumber, setPageNumber ] = useState(1);
     const [ pageLimit, setPageLimit] = useState(10);
     const [totalPages, setTotalPages ] = useState(10);
+    const [ bloodPressureRisk, setBloodPressureRisk] = useState<null | number>(null);
+    const [ evaluatedPatients, setEvaluatedPatients ] = useState<EvalPatient[]>([])
+   
 
     const callHandler = async (method:string) => {
         setMethod(method);
@@ -60,7 +74,6 @@ export default function TestHandler(){
                 method, 
                 headers: {
                     'Content-Type': 'application/json'
-
                 }
             });
 
@@ -69,6 +82,7 @@ export default function TestHandler(){
             setResponse(data);
             setPatients(data.patients);
             setLoading(true);
+            setInfoError('');
             setApiSuccess(true);
             setPageNumber(data.pagination.page);
             setPageLimit(data.pagination.limit);
@@ -84,22 +98,109 @@ export default function TestHandler(){
 
     useEffect(() => {      
         console.log("Patient Index: ",patientIndex);
+        console.log('Evaluated Patients:',evaluatedPatients );
         const callGetHandler = async () => {
            await callHandler('GET');
            return;
         };       
-        if(apiSuccess)
+        if(apiSuccess){            
             callGetHandler();
+           
+        }
     },[pageNumber,apiSuccess]);
 
-    useEffect(() => {        
-            console.log("Total Patients: ",totalPatients);
-    },[totalPatients]);
+    type pressureTypes = null | undefined | '';
+
+    useEffect(() => {
+        let thePatient:Patient;
+        if(patients?.length > 0){
+            const patient:Patient = patients[patientIndex as number];
+            thePatient= patient;
+            console.log("Current Patient: ", patient); 
+            try {
+                const pressure = patient.blood_pressure.split("/");
+                if( pressure === undefined )  // Throw Error for no values
+                    throw new TypeError(`Blood Pressure values (systolic and diastolic) are missing.`);
+
+                // Check values for null, undefined, or empty and throw Error
+                console.log("Pressure:", pressure);
+                const systolic = pressure[0];
+                const diastolic = pressure[1];
+                if( 
+                    (systolic === null || diastolic === null) ||
+                    (systolic === undefined || diastolic === undefined) ||
+                    (systolic === '' || diastolic === '')
+                   )
+                    throw new TypeError(`Blood Pressure Input values are null, undefined, or empty! ${JSON.stringify(pressure)}`);
+
+                // Check if values are convertible to Numbers and throw error
+                if(isNaN(Number(systolic)) || isNaN(Number(diastolic)))
+                    throw new TypeError(`Blood Pressure Input is not convertable to a number! ${JSON.stringify(pressure)}`);
+           
+                // Set State variables
+                const systolic_number = Number(pressure[0]);
+                const diastolic_number = Number(pressure[1]);
+                setSystolic(systolic_number);
+                setDiastolic(diastolic_number);
+
+                let normalPoints = 0;
+                let elevatedPoints = 0;
+                let stage1Points = 0;
+                let stage2Points = 0;
+                if(systolic_number < 120 && diastolic_number < 0)
+                    normalPoints = 0;
+                else if((systolic_number >= 120 && systolic_number <= 129) && diastolic_number < 80)
+                    elevatedPoints = 1;
+                else if((systolic_number >= 130 && systolic_number <= 139) || (diastolic_number >= 80 && diastolic_number <= 89))
+                    stage1Points = 2;
+                else if(systolic_number >= 140 || diastolic_number >= 90)
+                    stage2Points = 2;
+                const blood_pressure_risk = normalPoints + elevatedPoints + stage1Points + stage2Points;     
+
+                setBloodPressureRisk(blood_pressure_risk);
+                const eval_patient:EvalPatient = {
+                    patient_id: patient.patient_id,
+                    name: patient.name,
+                    age: patient.age,
+                    gender: patient.gender,
+                    blood_pressure: patient.blood_pressure,
+                    temperature: patient.temperature,
+                    visit_date: patient.visit_date,
+                    diagnosis: patient.diagnosis,
+                    medications: patient.medications,
+                    blood_pressure_risk: blood_pressure_risk,
+                    temperature_risk: 0,
+                    age_risk: 0,
+                    total_risk_score: blood_pressure_risk
+                }
+               
+                setEvaluatedPatients(prevItems => [...prevItems, eval_patient]);
+
+
+
+
+
+            } catch (error ) {
+                //console.error(`An error occurred:", ${(error as Error).message}`)
+                setInfoError(`An error occurred:", ${(error as Error).message} Patient: ${JSON.stringify(thePatient)} `);
+            }
+
+
+
+
+
+
+        }
+    },[patients, patientIndex])
+
+ 
+   
 
      const handleNextPatient = useCallback(() => {
-                const pIndex = patientIndex % pageLimit + 1;
+                const pIndex = patientIndex as number % pageLimit + 1;
                 const patient_number = ((pageNumber-1)*pageLimit)+pIndex+1;
                 setPatientNumber(patient_number);
+                setInfoError('');
                 console.log("Patient Number: ", patient_number);
                 setPatientIndex(pIndex);
                 if(pIndex*pageNumber === (pageLimit*pageNumber))
@@ -112,11 +213,12 @@ export default function TestHandler(){
             },[patientIndex]);
 
     const handlePreviousPatient = () => {
-        const pIndex = patientIndex % pageLimit;
+        const pIndex = patientIndex as number % pageLimit;
         const patient_number = ((pageNumber-1)*pageLimit)+pIndex;
         console.log("Patient Number: ", patient_number);
         const patients_lastPage = (pageNumber-1)*pageLimit;
         setPatientNumber(patient_number);
+        setInfoError('');
         setPatientIndex(pIndex-1);
         if(patient_number === patients_lastPage)
         {          
@@ -170,7 +272,7 @@ export default function TestHandler(){
                     <div className="grid grid-cols-1 md:grid-cols-4 gap-2">
                         {
                             patients?.map((patient:Patient, index:number) => (
-                                ( index === patientIndex &&
+                                ( index === patientIndex as number &&
 
                                     <div key={patient?.patient_id} className="text-center bg-white border border-gray-500 p-3 rounded-md shadow-sm hover:bg-gray-100">
                                             <div className="text-center  bg-gray-300 text-lg font-bold mb-5">{`Patient ${patientNumber} of ${totalPatients}`}</div>
@@ -202,11 +304,27 @@ export default function TestHandler(){
                                             <p className="font-normal text-left p-2 text-gray-700 dark:text-gray-400">{`Normal (Systolic <120 AND Diastolic <80): ${0} points`}</p>                                    
                                             <p className="font-normal text-left p-2 text-gray-700 dark:text-gray-400">{`Elevated (Systolic 120‑129 AND Diastolic <80): ${1} points`}</p>                                    
                                             <p className="font-normal text-left p-2 text-gray-700 dark:text-gray-400">{`Stage 1 (Systolic 130‑139 OR Diastolic 80‑89): ${2} points`}</p>                                    
-                                            <p className="font-normal text-left p-2 text-gray-700 dark:text-gray-400">{`Stage 2 (Systolic ≥140 OR Diastolic ≥90): ${3} points`}</p>                                    
-                                            <p className="font-normal text-left p-2 text-red-500 border-t border-gray-300 dark:text-gray-400">{`Invalid/Missing Data (0 points):`}</p>                                    
-                                            <p className="font-normal text-left text-sm p-2 text-gray-700 dark:text-gray-400">{`• Missing systolic or diastolic values ${'(e.g., "150/" or "/90")'}`}</p>                                    
-                                            <p className="font-normal text-left text-sm p-2 text-gray-700 dark:text-gray-400">{`• Non-numeric values ${'(e.g., "INVALID", "N/A")'}`}</p>                                    
-                                            <p className="font-normal text-left text-sm p-2 text-gray-700 dark:text-gray-400">{`• Null, undefined, or empty values`}</p>                                    
+                                            <p className="font-normal text-left p-2 text-gray-700 dark:text-gray-400">{`Stage 2 (Systolic ≥140 OR Diastolic ≥90): ${3} points`}</p> 
+                                            { bloodPressureRisk !== null &&
+                                                <p className="font-normal text-left p-2 text-blue-500 border-t border-gray-300 dark:text-gray-400">{`The Blood Pressure Risk for ${patient.name} is ${bloodPressureRisk}.`}</p>                                    
+                                                
+                                            }   
+                                             { bloodPressureRisk !== null && bloodPressureRisk >= 4 &&
+                                                <p className="font-normal text-left p-2 text-blue-500 border-t border-gray-300 dark:text-gray-400">{`The Blood Pressure Risk for ${patient.name} is ${bloodPressureRisk}.`}</p>                                    
+                                                
+                                            }                                      
+                                            <p className="font-normal text-left p-2 text-red-500 border-t border-gray-300 dark:text-gray-400">{`Invalid/Missing Data (0 points):`}</p>  
+                                           
+                                            { infoError === '' &&
+                                            <div>
+                                                <p className="font-normal text-left text-sm p-2 text-gray-700 dark:text-gray-400">{`• Missing systolic or diastolic values ${'(e.g., "150/" or "/90")'}`}</p>                                    
+                                                <p className="font-normal text-left text-sm p-2 text-gray-700 dark:text-gray-400">{`• Non-numeric values ${'(e.g., "INVALID", "N/A")'}`}</p>                                    
+                                                <p className="font-normal text-left text-sm p-2 text-gray-700 dark:text-gray-400">{`• Null, undefined, or empty values`}</p>  
+                                            </div>
+                                            }
+                                            { infoError !== '' &&
+                                                <p className="font-normal overflow-y-auto  text-left text-sm p-6 text-red-500 dark:text-gray-400">{infoError}</p>
+                                            }                                  
                                           
                                     </div>
                                 )                               
@@ -259,6 +377,7 @@ export default function TestHandler(){
 
 
                 }
+                
                
         </>
         
